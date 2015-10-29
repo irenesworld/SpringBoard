092d57f2acdf56758b7d93dd36f9a5971cf8ce06
Dan <kramer425@gmail.com>
2015-10-29 18:12:04 -0400

    Connect

---
+++ HomePage.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Dan
+ * Date: 10/29/15
+ * Time: 5:26 PM
+ */
+
+
+
+
+?>
---
+++ connect.php
@@ -0,0 +1,35 @@
+<?php
+
+
+$DB_USERNAME =  'root';
+$DB_PASSWORD = 'springboard123';
+$DB_HOSTNAME = 'springboard.c4psj6yhsvss.us-east-1.rds.amazonaws.com';
+$DB_NAME = 'springboarddb';
+$DB_PORT = '8008';
+$conn = '';
+
+function connect(){
+
+    global $DB_USERNAME, $DB_PASSWORD, $DB_HOSTNAME, $DB_NAME, $DB_PORT, $conn;
+    $conn = mysqli_connect($DB_HOSTNAME, $DB_USERNAME, $DB_PASSWORD, $DB_NAME, $DB_PORT);
+
+    if(!$conn){
+        die('Error: Could not connect to database.');
+    }else {
+        echo 'conncted';
+    }
+}
+
+function close(){
+    global $conn;
+    $conn = null;
+
+    if(!$conn){
+        echo 'closed con';
+    }
+}
+
+//connect();
+//close();
+
+?>
