35e6427d98f5504c559cca05bca133ba17db7968
Dan <kramer425@gmail.com>
2015-10-31 18:05:47 -0400

    Fixed create user function

--- HomePage.php
+++ HomePage.php
@@ -1,13 +1,26 @@
 <?php
-    include 'connect.php';
-
-    echo 'hello';
-    connect();
+    require 'connect.php';
+error_reporting(E_ALL);
+ini_set('display_errors', '1');
 
     function createUser($name, $email, $password, $major, $univeristyID){
-        $idStatment = "insert into user (iduser, email, password, name, total_votes, major, pictureURL, universityID) VALUES(DEFAULT, $email, $password, $name, 0, $major, DEFAULT, $univeristyID)";
-        $return = mysqli_query($idStatment);
+        connect();
+        global $conn;
+
+        $query = "INSERT INTO user (email, password, userName, total_votes, major, pictureURL) VALUES(?, ?, ?, ?, ?, ?)";
+        echo ' before prepare';
+        $statment = mysqli_prepare($conn,$query);
+        if ( !$statment ) {
+            die('mysqli error: '.mysqli_error($conn));
+        }
+        $votes = 0;
+        mysqli_stmt_bind_param($statment, 'sssisi', $email, $password, $name, $votes, $major, $univeristyID);
+        mysqli_stmt_execute($statment);
+
+        printf("%d Row inserted.\n", mysqli_stmt_affected_rows($statment));
+        mysqli_stmt_close($statment);
+        close();
     }
 
-    close();
+    createUser('dk','djk316@lehigh.edu','password','Comp Sci',1);
 ?>
