e59aae3fa13067fba3427d1ed65d5331fef308d8
Dan <kramer425@gmail.com>
2015-11-01 17:53:02 -0500

    Added error messages to homepage.php

--- login.php
+++ login.php
@@ -15,8 +15,24 @@ function logout(){
     $membership->logout();
 }
 
-if(isset($_POST['login'])){
-    login($_POST['email'],$_POST['password']);
+if(isset($_POST['signin'])){
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+
+    if(!$_POST['email'] || !filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)){
+        $errorEmail = 'Please enter a valid email';
+    }
+
+    if(!$_POST['password']){
+        $errorPassword = 'Please enter a passsword';
+    }
+
+    if(!$errorEmail && !$errorPassword){
+        login($email,$password);
+    }
 }
+
+
+
 
 ?>
