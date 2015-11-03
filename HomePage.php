f45078b4b21a21ba92aab8f8cd02f4f0a0deb1ed
Irene Lau <irl216@lehigh.edu>
2015-11-03 02:23:25 -0500

    Fixed a couple of typos

--- HomePage.php
+++ HomePage.php
@@ -2,7 +2,7 @@
     require 'connect.php';
     require 'login.php';
 
-    function createUser($name, $email, $password, $major, $univeristyID){
+    function createUser($name, $email, $password, $major, $universityID){
         connect();
         global $conn;
 
@@ -13,7 +13,7 @@
             die('mysqli error: '.mysqli_error($conn));
         }
         $votes = 0;
-        mysqli_stmt_bind_param($statment, 'sssisi', $email, $password, $name, $votes, $major, $univeristyID);
+        mysqli_stmt_bind_param($statment, 'sssisi', $email, $password, $name, $votes, $major, $universityID);
         mysqli_stmt_execute($statment);
 
         if(mysqli_stmt_num_rows($statment) > 0){
@@ -40,7 +40,7 @@
         }
 
         if(!$_POST['password']){
-            $errorPassword = 'Please enter a passsword';
+            $errorPassword = 'Please enter a password';
         }
 
         if(!$_POST['major']){
