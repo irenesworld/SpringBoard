b2538a731e26762d2cc849e32899b7ad9ced4d83
Irene Lau <irl216@lehigh.edu>
2015-11-19 16:26:48 -0500

    Connected the old password variable between HTML & PHP files

--- login.php
+++ login.php
@@ -6,9 +6,14 @@ require 'Membership.php';
 $membership = new Membership();
 
 function login($email, $password){
-    echo ' in login of login php';
+    echo 'LOGGING IN  ';
+    echo "\r\n";
     global $membership;
     $response = $membership->login($email, $password);
+    $_SESSION["userEmail"] = $email;
+    echo '  email for session: ';
+    echo $email;
+    echo "\r\n";
 }
 
 function logout(){
