2634275b5a6c2e9005503ec25863ec2c737cc426
Dan <kramer425@gmail.com>
2015-11-28 16:02:23 -0500

    added view resumes

---
+++ reviewresumes.php
@@ -0,0 +1,83 @@
+<?php
+// hi kramer
+
+require_once 'connect.php';
+require_once 'login.php';
+
+$resumeArray = array();
+viewByTimeStamp();
+
+function viewByTimeStamp(){
+    connect();
+    global $conn;
+    $query = "SELECT ts, user.username, resume.name, resumeURL from user natural join resume order by ts DESC";
+    $statment = mysqli_prepare($conn, $query);
+    if ( !$statment ) {
+        die('mysqli error: '.mysqli_error($conn));
+    }
+
+    mysqli_stmt_execute($statment);
+
+
+    $timeStamp = "";
+    $userName = "";
+    $resumeName = "";
+    $resumeURL = "";
+    mysqli_stmt_bind_result($statment, $timeStamp, $userName, $resumeName, $resumeURL);
+
+    while($row = mysqli_stmt_fetch($statment)){
+        $resumeArray[] = array($timeStamp, $userName, $resumeName, $resumeURL);
+        foreach($resumeArray as &$row2) {
+            foreach ($row2 as &$str) {
+                echo "<a href='#' class='list-group-item'>";
+                echo "<table><tr><td style='padding-right:50px'>";
+                echo $row2[0];
+                echo "</td>";
+                echo "<td>" . $row2[1] . "</td>";
+                echo "<td>" . $row2[2] . "</td>";
+                echo "</tr></table>";
+            }
+        }
+    }
+
+    mysqli_stmt_close($statment);
+    close();
+}
+
+function viewByMajor($major){
+    connect();
+    global $conn;
+    $query = "SELECT ts, user.username, resume.name, resumeURL from user natural join resume where major = ? order by ts DESC";
+    $statment = mysqli_prepare($conn, $query);
+    if ( !$statment ) {
+        die('mysqli error: '.mysqli_error($conn));
+    }
+    mysqli_stmt_bind_param($statment, 's', $major);
+    mysqli_stmt_execute($statment);
+
+    $timeStamp = "";
+    $userName = "";
+    $resumeName = "";
+    $resumeURL = "";
+    mysqli_stmt_bind_result($statment, $timeStamp, $userName, $resumeName, $resumeURL);
+
+    while($row = mysqli_stmt_fetch($statment)){
+        $resumeArray[] = array($timeStamp, $userName, $resumeName, $resumeURL);
+        foreach($resumeArray as &$row2) {
+            foreach ($row2 as &$str) {
+                echo "<a href='#' class='list-group-item'>";
+                echo "<table><tr><td style='padding-right:50px'>";
+                echo $row2[0];
+                echo "</td>";
+                echo "<td>" . $row2[1] . "</td>";
+                echo "<td>" . $row2[2] . "</td>";
+                echo "</tr></table>";
+            }
+        }
+    }
+
+    mysqli_stmt_close($statment);
+    close();
+}
+
+?>
