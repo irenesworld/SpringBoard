5e02d2215bc08675551b2c5f561a2fbd51587f91
Irene Lau <irl216@lehigh.edu>
2015-11-29 06:31:23 -0500

    Finished some HTML code for comment box

--- viewresume.php
+++ viewresume.php
@@ -109,6 +109,44 @@ function getAllCommentsAndVotes($resumeID){
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
     <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
 
+    <style>
+        .thumbnail {
+            padding:0px;
+            border-style:none;
+        }
+        .thumbnail img {
+            border-radius:50%;
+            border-style:none;
+        }
+        .panel {
+            position:relative;
+        }
+        .panel>.panel-heading:after,.panel>.panel-heading:before{
+            position:absolute;
+            top:11px;left:-16px;
+            right:100%;
+            width:0;
+            height:0;
+            display:block;
+            content:" ";
+            border-color:transparent;
+            border-style:solid solid outset;
+            pointer-events:none;
+        }
+        .panel>.panel-heading:after{
+            border-width:7px;
+            border-right-color:#f7f7f7;
+            margin-top:1px;
+            margin-left:2px;
+        }
+        .panel>.panel-heading:before{
+            border-right-color:#ddd;
+            border-width:8px;
+        }
+        .form-control{
+            height: 142px;
+        }
+    </style>
 </head>
 
 <body>
@@ -154,6 +192,84 @@ function getAllCommentsAndVotes($resumeID){
         }
     ?>
     <div type="filepicker-preview" data-fp-url="<?php echo $resumeURL ?>" style="text-align:center width:75%; height:500px"> </div>
+
+    <!-- populating the comments -->
+
+    <!-- adding comments -->
+    <div class="row">
+        <div class="col-sm-12">
+            <br>
+            <h3>Comments</h3>
+            <br>
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col-sm-2">
+            <div class="thumbnail">
+                <img class="img-responsive user-photo" src="https://www.filepicker.io/api/file/f107zsqaRgC9YQg3gYlG/convert?crop=0,280,718,718">
+            </div>
+        </div>
+        <div class="col-sm-10">
+            <div class="panel panel-default">
+                <div class="panel-heading">
+                    <strong>Irene Lau&nbsp;</strong>
+                    <span class="text-muted">&nbsp; Dec. 9, 2015&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span>
+                </div>
+                <div class="panel-body">
+                    Comment. Mac & Cheese. Pepperoni Pizza. This resume is awesomeeeeeeeee. Superb. Beautiful. Magnificant.
+                </div>
+                <div class="panel-footer" style="text-align:right">
+                    <span class="glyphicon glyphicon-chevron-up"></span>
+                    50
+                    <span class="glyphicon glyphicon-chevron-down"></span>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-sm-2">
+            <div class="thumbnail">
+                <img class="img-responsive user-photo" src="https://www.filepicker.io/api/file/Ru0Edq4QGqLhLGSGOv8y/convert?crop=210,0,540,540">
+            </div>
+        </div>
+        <div class="col-sm-10">
+            <div class="panel panel-default">
+                <div class="panel-heading">
+                        <strong>Dan Kramer&nbsp;</strong>
+                        <span class="text-muted">&nbsp; Dec. 9, 2015 &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span>
+                </div>
+                <div class="panel-body">
+                    Helloooooooooooooooo. Hiiiiiiiiiiiiiiiiiiii. Longgggggggggggg comment. Longerrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrr comment.
+                    LongggggggggGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGG comment. who lives under the pineapple under the sea? spongebob squarepaaaaaaants.
+                    aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaabbbbbbbbbbbbbbbbbbbbbbbbb
+                </div>
+                <div class="panel-footer" style="text-align:right">
+                    <span class="glyphicon glyphicon-chevron-up"></span>
+                    150
+                    <span class="glyphicon glyphicon-chevron-down"></span>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-sm-2">
+            <div class="thumbnail">
+                <img class="img-responsive user-photo" src="https://www.filepicker.io/api/file/TovycatBT52d3SCCIO93/convert?crop=341,0,1366,1366">
+            </div>
+        </div>
+        <div class="col-sm-10">
+            <form class="form" role="form">
+                <div class="form-group">
+                    <textarea class="form-control" rows="3" placeholder="Comment"></textarea>
+                </div>
+                <div class="form-group" align="right">
+                    <button class="btn btn-default">Add</button>
+                </div>
+            </form>
+        </div>
+    </div>
+
 
 </div>
 
