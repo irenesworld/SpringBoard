2daf2d1d2ed372b4abec49281323c6ad62566d03
Irene Lau <irl216@lehigh.edu>
2015-12-04 05:51:47 -0500

    Works with new files

--- viewresume.php
+++ viewresume.php
@@ -16,49 +16,66 @@ if(!(isLoggedIn())){
 
 
 
-if(isset($_POST['addbutton']))
-{
-    $comment = htmlspecialchars($_POST['commentStr']);
-    echo $comment;
-
+$resumeURL = "";
+if (isset($_GET['resumeURL'])) {
+    $resumeURL = htmlspecialchars($_GET['resumeURL']);
+    $_SESSION['resumeURL'] = $resumeURL;
+}else {
+    $resumeURL = $_SESSION['resumeURL'];
+}
+$idresume = "";
+if (isset($_GET['idresume'])) {
+    $idresume = htmlspecialchars($_GET['idresume']);
+    $_SESSION['idresume'] = $idresume;
+}else{
+    $idresume =  $_SESSION['idresume'];
 }
 
+if ('POST' == $_SERVER['REQUEST_METHOD']) {
+    if (isset($_POST['addComment'])) {
+        $commentStr = htmlspecialchars($_POST['message']);
+        echo $commentStr;
+        echo $idresume;
+
+        connect();
+        global $conn;
+        $query = "INSERT INTO comment (author_id, resume_id, commentString) VALUES (?,?,?);";
+        $statment = mysqli_prepare($conn, $query);
+        if (!$statment) {
+            die('mysqli error: ' . mysqli_error($conn));
+        }
+
+        mysqli_stmt_bind_param($statment, 'iis', $_SESSION['userid'], $idresume, $commentStr);
+        mysqli_stmt_execute($statment);
+
+        $newCommentID = mysqli_stmt_insert_id($statment);
+
+        mysqli_stmt_close($statment);
+
+        echo 'hi';
+        // makes a positive vote for youself. Reddit style
+        $positiveVote = 1;
+        $query2 = "insert into vote (idu, idc, positive) values(?,?,?)";
+        $statment2 = mysqli_prepare($conn, $query2);
+        if (!$statment2) {
+            die('mysqli error: ' . mysqli_error($conn));
+        }
+        echo 'hello';
+        mysqli_stmt_bind_param($statment2, 'iii', $_SESSION['userid'], $newCommentID, $positiveVote);
+        mysqli_stmt_execute($statment2);
+
+        mysqli_stmt_close($statment2);
+        echo 'hm';
+        close();
+    }
+}
 
 $resumeArray = array();
 
 //addCommentToResume(15, "Test comment. Do not upvote");
-function addCommentToResume($resumeID, $commentStr){
-    connect();
-    global $conn;
-    $query = "insert into comment (author_id, resume_id, commentString) values(?,?,?);";
-    $statment = mysqli_prepare($conn, $query);
-    if ( !$statment ) {
-        die('mysqli error: '.mysqli_error($conn));
-    }
+/*function addCommentToResume($resumeID, $commentStr){
 
-    mysqli_stmt_bind_param($statment, 'iis', $_SESSION['userid'], $resumeID, $commentStr);
-    mysqli_stmt_execute($statment);
-
-    $newCommentID = mysqli_stmt_insert_id($statment);
-
-    mysqli_stmt_close($statment);
-
-
-    // makes a positive vote for youself. Reddit style
-    $positiveVote = 1;
-    $query2 = "insert into vote (idu, idc, positive) values(?,?,?)";
-    $statment2 = mysqli_prepare($conn, $query2);
-    if ( !$statment2 ) {
-        die('mysqli error: '.mysqli_error($conn));
-    }
-
-    mysqli_stmt_bind_param($statment2, 'iii', $_SESSION['userid'], $newCommentID, $positiveVote);
-    mysqli_stmt_execute($statment2);
-
-    mysqli_stmt_close($statment2);
-
-    close();
-}
+}*/
 
 function addVote($commentID, $boolean){
     connect();
@@ -101,184 +118,6 @@ function addVote($commentID, $boolean){
 
     close();
 }
-
-
-/*
-function addCommentToComment($resumeID, $commentID, $commentStr){
-    connect();
-    global $conn;
-    $query = "insert into comment (author_id, resume_id, parent_id, commentString) values(?,?,?,?); SELECT LAST_INSERT_ID();";
-    $statment = mysqli_prepare($conn, $query);
-    if ( !$statment ) {
-        die('mysqli error: '.mysqli_error($conn));
-    }
-
-    mysqli_stmt_bind_param($statment, 'iiis', $_SESSION['userid'], $resumeID, $commentID, $commentStr);
-    mysqli_stmt_execute($statment);
-
-    $newCommentID = "";
-    mysqli_stmt_bind_result($statment, $newCommentID);
-
-    mysqli_stmt_close($statment);
-
-    // makes a positive vote for youself. Reddit style
-    $positiveVote = 1;
-    $query2 = "insert into vote (idu, idc, positive) values(?,?,?)";
-    $statment2 = mysqli_prepare($conn, $query2);
-    if ( !$statment2 ) {
-        die('mysqli error: '.mysqli_error($conn));
-    }
-
-    mysqli_stmt_bind_param($statment2, 'iii', $_SESSION['userid'], $newCommentID, $positiveVote);
-    mysqli_stmt_execute($statment2);
-
-    $result2 = "";
-    mysqli_stmt_bind_result($statment2, $result2);
-
-    mysqli_stmt_close($statment2);
-
-    close();
-}
-*/
-
-//getAllCommentsAndVotes();
-/*
-function getAllCommentsAndVotes()
-{
-    try {
-        connect();
-        global $conn;
-
-        $query = "SELECT idcomment, author_id, comment.ts, commentString, iduser, userName, major, pictureURL, university.name from comment, user, university, resume where idresume = ? and author_id = iduser and universityID = iduniversity and idresume = resume_id order by ts";
-
-        $statment = mysqli_prepare($conn, $query);
-        if (!$statment) {
-            die('mysqli error: ' . mysqli_error($conn));
-        }
-
-        $idresume = "";
-        if (isset($_GET['idresume'])) {
-            $idresume = htmlspecialchars($_GET['idresume']);
-            $_SESSION['idresume'] = $idresume;
-        }else{
-            $idresume =  $_SESSION['idresume'];
-        }
-        echo 'id resyme is '. $idresume;
-
-        mysqli_stmt_bind_param($statment, 'i', $idresume);
-        mysqli_stmt_execute($statment);
-
-        $idcomment = "";
-        $authorID = "";
-        $parent_ID = "";
-        $ts = "";
-        $commentString = "";
-        $iduser = "";
-        $userName = "";
-        $major = "";
-        $pictureURL = "";
-        $universityName = "";
-
-        mysqli_stmt_bind_result($statment, $idcomment, $authorID, $ts, $commentString, $iduser, $userName, $major, $pictureURL, $universityName);
-
-        $rCounter = 0;
-        while (mysqli_stmt_fetch($statment)) {
-            $resumeArray[$rCounter] = array($idcomment, $authorID, $ts, $commentString, $iduser, $userName, $major, $pictureURL, $universityName);
-            $rCounter++;
-        }
-
-        mysqli_stmt_close($statment);
-
-
-        if (!empty($resumeArray)) {
-            for ($i = 0; $i < $rCounter; $i++) {
-                $query2 = "select * from vote where idc = ?";
-
-
-                $statment2 = mysqli_prepare($conn, $query2);
-                if (!$statment2) {
-                    die('mysqli error: ' . mysqli_error($conn));
-                }
-
-                mysqli_stmt_bind_param($statment2, 'i', $resumeArray[$i][0]);
-                mysqli_stmt_execute($statment2);
-
-                $idc = "";
-                $idu = "";
-                $postive = "";
-                mysqli_stmt_bind_result($statment2, $idc, $idu, $postive);
-
-                $vCounter = 0;
-                while (mysqli_stmt_fetch($statment2)) {
-                    $voteArray[$vCounter] = array($idc, $idu, $postive);
-                    $vCounter++;
-                }
-                mysqli_stmt_close($statment2);
-
-                if (!empty($voteArray)) {
-                    $totalUpVote = 0;
-                    $totalDownVote = 0;
-
-                    for ($j = 0; $j < $vCounter; $j++) {
-                        if ($voteArray[$j][2] == 1) {
-                            $totalUpVote++;
-                        } else {
-                            $totalDownVote++;
-                        }
-                    }
-
-                    $totalVotes = $totalUpVote - $totalDownVote;
-                } else {
-                    $totalVotes = 0;
-                }
-                array_push($resumeArray[$i], $totalVotes);
-            }
-
-            // echo "<br> Resume: ".end($resumeArray)[0]." ".end($resumeArray)[1]." ".end($resumeArray)[2]." ".end($resumeArray)[3]." ".end($resumeArray)[4]." ".end($resumeArray)[5]." ".end($resumeArray)[6]." ".end($resumeArray)[7]." ".end($resumeArray)[8]." ".end($resumeArray)[9]. "<br>";
-
-            $stri = "";
-            //ob_start();
-            for ($i = 0; $i < $rCounter; $i++) {
-                $stri .= '<div class=\'row\'>
-                            <div class=\'col-sm-2\'>
-                                <div class=\'thumbnail\'>
-                                    <img class=\'img-responsive user-photo\' src= ' . $resumeArray[$i][7] . '>
-                                </div>
-                            </div>
-                            <div class=\'col-sm-10\'>
-                                <div class=\'panel panel-default\'>
-                                    <div class=\'panel-heading\'>
-                                        <strong> ' . $resumeArray[$i][5] . ' &nbsp;</strong>
-                                            <span class=\'text-muted\'>&nbsp; ' . $resumeArray[$i][2] . '&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span>
-                                    </div>
-                                    <div class=\'panel-body\'>
-                                       ' . $resumeArray[$i][3] . '
-                                    </div>
-                                    <div class=\'panel-footer\'style=\'text-align:right\'>
-                                        <span class=\'glyphicon glyphicon-chevron-up\'></span>
-                                          ' . $resumeArray[$i][9] . '
-                                        <span class=\'glyphicon glyphicon-chevron-down\'></span>
-                                    </div>
-                                </div>
-                            </div>
-                         </div>';
-
-            }
-
-            //$value = ob_get_contents();
-            //ob_end_clean();
-            //echo $stri;
-            //var_dump($stri);
-            echo ' another line ';
-
-        } else {
-            echo 'There are no commments to display';
-        }
-    }catch (Exception $e){
-        echo $e->getMessage();
-    }
-}
-*/
 
 ?>
 
@@ -376,20 +215,8 @@ function getAllCommentsAndVotes()
 <div class="container">
     <br>
     <br>
-    <?php
-        $resumeURL = "";
-        if (isset($_GET['resumeURL'])) {
-            $resumeURL = htmlspecialchars($_GET['resumeURL']);
-            $_SESSION['resumeURL'] = $resumeURL;
-        }else{
-            $resumeURL =  $_SESSION['resumeURL'];
-        }
-    ?>
+
     <div type="filepicker-preview" data-fp-url="<?php echo $resumeURL ?>" style="text-align:center width:75%; height:500px"> </div>
-
-    <!-- populating the comments -->
-
-    <!-- adding comments -->
     <div class="row">
         <div class="col-sm-12">
             <br>
@@ -397,6 +224,7 @@ function getAllCommentsAndVotes()
             <br>
         </div>
     </div>
+    <div class="populating">
     <?php  try {
         connect();
         global $conn;
@@ -406,14 +234,6 @@ function getAllCommentsAndVotes()
         $statment = mysqli_prepare($conn, $query);
         if (!$statment) {
             die('mysqli error: ' . mysqli_error($conn));
-        }
-
-        $idresume = "";
-        if (isset($_GET['idresume'])) {
-            $idresume = htmlspecialchars($_GET['idresume']);
-            $_SESSION['idresume'] = $idresume;
-        }else{
-            $idresume =  $_SESSION['idresume'];
         }
 
         mysqli_stmt_bind_param($statment, 'i', $idresume);
@@ -485,8 +305,6 @@ function getAllCommentsAndVotes()
                 array_push($resumeArray[$i], $totalVotes);
             }
 
-            // echo "<br> Resume: ".end($resumeArray)[0]." ".end($resumeArray)[1]." ".end($resumeArray)[2]." ".end($resumeArray)[3]." ".end($resumeArray)[4]." ".end($resumeArray)[5]." ".end($resumeArray)[6]." ".end($resumeArray)[7]." ".end($resumeArray)[8]." ".end($resumeArray)[9]. "<br>";
-
             $stri = "";
             ob_start();
             for ($i = 0; $i < $rCounter; $i++) {
@@ -518,14 +336,7 @@ function getAllCommentsAndVotes()
             $value = ob_get_contents();
             ob_end_clean();
             echo $value;
-
-            echo ' another line ';
-
-            //$value = ob_get_contents();
-            //ob_end_clean();
-            //echo $stri;
             var_dump($stri);
-            echo ' another line ';
 
         } else {
             echo 'There are no commments to display';
@@ -533,37 +344,6 @@ function getAllCommentsAndVotes()
     }catch (Exception $e){
         echo $e->getMessage();
     }?>
-    <?php echo ' the next line in the html part '; ?>
-
-    <?php echo ' after echo'; ?>
-
-
-    <?php echo ' after the nect comment part why doesnt this print'; ?>
-
-    <div class="row">
-        <div class="col-sm-2">
-            <div class="thumbnail">
-                <img class="img-responsive user-photo" src="https://www.filepicker.io/api/file/Ru0Edq4QGqLhLGSGOv8y/convert?crop=210,0,540,540">
-            </div>
-        </div>
-        <div class="col-sm-10">
-            <div class="panel panel-default">
-                <div class="panel-heading">
-                        <strong>Dan Kramer&nbsp;</strong>
-                        <span class="text-muted">&nbsp; Dec. 9, 2015 &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span>
-                </div>
-                <div class="panel-body">
-                    Helloooooooooooooooo. Hiiiiiiiiiiiiiiiiiiii. Longgggggggggggg comment. Longerrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrr comment.
-                    LongggggggggGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGG comment. who lives under the pineapple under the sea? spongebob squarepaaaaaaants.
-                    aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaabbbbbbbbbbbbbbbbbbbbbbbbb
-                </div>
-                <div class="panel-footer" style="text-align:right">
-                    <span class="glyphicon glyphicon-chevron-up"></span>
-                    150
-                    <span class="glyphicon glyphicon-chevron-down"></span>
-                </div>
-            </div>
-        </div>
     </div>
     <div class="row">
         <div class="col-sm-2">
@@ -572,17 +352,14 @@ function getAllCommentsAndVotes()
             </div>
         </div>
         <div class="col-sm-10">
-
+            <form role="form" method="post" action=<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?> >
+                <div class="form-group">
+                    <textarea name="message" rows="3" id="message" required></textarea>
+                </div>
+                <button type="submit" class="btn btn-primary pull-right" name="addComment">Add</button>
+            </form>
         </div>
     </div>
-
-
-
 </div>
-
-
-
 </body>
-
-
 </html>
